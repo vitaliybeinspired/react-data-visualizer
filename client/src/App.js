b435ec6bb806c7758f40ab588c2b422a48fed001
Naoki <Naoki.l.95957@gmail.com>
2021-04-23 12:03:47 -0700

    adding simple globe test

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import {LandingPage} from './pages/LandingPage'
+import {MyGlobe} from './pages/GlobeTest'
 
 function App() {
   return (
     <div className='App'>
         <Router>
-          <Route exact path ='/' component = {LandingPage}/>
+          <Route exact path ='/land' component = {LandingPage}/>
+          <Route exact path ='/' component = {MyGlobe}/>
           {/*we can add more path's here*/}
         </Router>      
     </div>    
