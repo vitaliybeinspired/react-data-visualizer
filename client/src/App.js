5e92fae227099002f159ba945ac4985f2226c816
Naoki <Naoki.l.95957@gmail.com>
2021-05-09 22:41:18 -0700

    updated packages and nested-git to use heroku

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,19 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import {LandingPage} from './pages/LandingPage';
 import {About} from './pages/About';
 import {ExportData} from './pages/ExportData';
-import BrowserRouter from 'react-router-dom/BrowserRouter';
+import axios from 'axios';
+
+// This will be our new proxy
+axios.defaults.baseURL = "https://cfv-server.herokuapp.com/"
 
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter>
-          <Router basename={process.env.PUBLIC_URL}>
-            <Route exact path ='/' component = {LandingPage}/>
-            <Route exact path ='/about' component = {About}/>
-            <Route exact path ='/exportdata' component = {ExportData}/>
-          </Router>
-      </BrowserRouter>
+        <Router>
+          <Route exact path ='/' component = {LandingPage}/>
+          <Route exact path ='/about' component = {About}/>
+          <Route exact path ='/exportdata' component = {ExportData}/>
+        </Router>
     </div>    
   );
 }
