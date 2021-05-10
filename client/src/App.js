ac8b9e73cd484056ec063728696f4e72efe93d01
Naoki <Naoki.l.95957@gmail.com>
2021-05-10 11:57:47 -0700

    added heroku settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {LandingPage} from './pages/LandingPage';
 import {About} from './pages/About';
 import {ExportData} from './pages/ExportData';
 import axios from 'axios';
 
-// This will be our new proxy
-axios.defaults.baseURL = "https://cfv-server.herokuapp.com/"
+// These will be our proxies
+// this is for backend axios requests
+axios.defaults.baseURL = "https://cfv-server.herokuapp.com/";
 
 function App() {
   return (
     <div className='App'>
         <Router>
-          <Route exact path ='/' component = {LandingPage}/>
-          <Route exact path ='/about' component = {About}/>
-          <Route exact path ='/exportdata' component = {ExportData}/>
+          <Switch>
+            <Route exact path ='/' component = {LandingPage}/>
+            <Route exact path ='/about' component = {About}/>
+            <Route exact path ='/exportdata' component = {ExportData}/>
+          </Switch>
         </Router>
     </div>    
   );
